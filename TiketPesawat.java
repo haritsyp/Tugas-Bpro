01b33dd6437584725cf88d19c21a70431d52b1e2
haritsyp <14410100076@stikom.edu>
2015-04-09 20:06:20 +0700

    Update TiketPesawat.java

--- TiketPesawat.java
+++ TiketPesawat.java
@@ -9,164 +9,220 @@ public class TiketPesawat
     int pesan=0;
     int jumlah=0;
     int total=0;
+    int harga=0;
     String[]kolom = {"A","B","C","D","E","F"};
-    String[] baris = {"1", "2", "3", "4","5","6","7","8","9","10","11","12","13"};
+    String[] baris = {" 1", " 2", " 3", " 4"," 5"," 6"," 7"," 8"," 9","10","11","12","13"};
     int[][] kursi = 
-      {{0,0,1,0,1,1},
-      {0,1,0,1,0,1},
-      {0,0,1,1,0,1},
-      {1,0,1,0,1,1},
-      {0,1,0,1,0,0},
-      {0,1,0,0,0,1},
-      {1,0,0,0,1,1},
-      {0,1,0,1,1,0},
-      {1,0,1,1,0,1},
-      {0,1,0,1,1,1},
-      {0,0,1,0,1,0},
-      {0,0,1,1,0,1},
-      {0,0,0,0,1,0}};
-
-    int[][] nKursi = 
-     {{0,0,1,0,1,1},
-      {0,1,0,1,0,1},
-      {0,0,1,1,0,1},
-      {1,0,1,0,1,1},
-      {0,1,0,1,0,0},
-      {0,1,0,0,0,1},
-      {1,0,0,0,1,1},
-      {0,1,0,1,1,0},
-      {1,0,1,1,0,1},
-      {0,1,0,1,1,1},
-      {0,0,1,0,1,0},
-      {0,0,1,1,0,1},
-      {0,0,0,0,1,0}};
-
+    {{0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0},
+    {0,0,0,0,0,0}};
     
-    System.out.println("Baris/Kolom\tA\tB\tC\tD\tE\tF");
-    for(int i=0;i<13;i++)
+    try
     {
-      System.out.print("baris "+baris[i]+"\t");
-      for(int j=0;j<6;j++)
-      {
-        if(kursi[i][j]==0)
-        {
-          System.out.print("*\t");
-        }
-        else
-        {
-        System.out.print("X\t");
-        }
-      }
-      System.out.println();
-    }
-    System.out.println();
-    do{
-    System.out.println("1. Penjualan Tiket");
-    System.out.println("2. Daftar Kursi Kosong");
-    System.out.println("3. Daftar Kursi terisi");
-    System.out.println("4. Jumlah Semua kursi");
-    System.out.println("5. Omset");
-    System.out.println("6. Keluar");        
-    System.out.print("SILAHKAN PILIH MENU YANG ANDA INGINKAN[1-5]: ");
-    int pilihan=br.nextInt();  
+      do{
+        System.out.println("1. Penjualan Tiket");
+        System.out.println("2. Daftar Kursi");
+        System.out.println("3. Jumlah Kursi Terisi");
+        System.out.println("4. Jumlah Kursi Kosong");
+        System.out.println("5. Omset");
+        System.out.println("6. Keluar");        
+        System.out.print("SILAHKAN PILIH MENU YANG ANDA INGINKAN[1-5]: ");
+        int pilihan=br.nextInt();  
         
-    switch(pilihan) {
-    case 1: 
-      
-      System.out.print("baris ke : ");
-      int bariskursi=br.nextInt();  
-      System.out.print("kolom ke : ");
-      int kolomkursi=br.nextInt(); 
-           
-         if(nKursi[bariskursi][kolomkursi] == 0)
-         {
-         System.out.println("Kursi Tersedia");
-         }
-         else if(nKursi[bariskursi][kolomkursi] == 1)
-         {
-         System.out.println("Maaf kursi telah dipesan");
-         }      
-     
-    if(nKursi[bariskursi][kolomkursi] == 0)
+        switch(pilihan) {
+          case 1: 
+            
+            System.out.print("baris ke : ");
+            int bariskursi=(br.nextInt()-1);  
+            System.out.print("kolom ke : ");
+            int kolomkursi=(br.nextInt()-1); 
+            
+            if(kursi[bariskursi][kolomkursi] == 0)
+            {
+              System.out.println("Kursi Tersedia");
+            }
+            else if(kursi[bariskursi][kolomkursi] == 1)
+            {
+              System.out.println("Maaf kursi telah dipesan");
+            }      
+            
+            if(kursi[bariskursi][kolomkursi] == 0)
+            {
+              if(bariskursi==0||bariskursi==1)
+              {
+                harga=175000;
+                System.out.print("Harga Tiket : Rp."+harga);
+              }
+              else if(bariskursi>=2&&bariskursi<=6)
+              {
+                harga=150000;
+                System.out.print("Harga Tiket Rp."+harga);
+              }
+              else
+              {
+                harga=100000;
+                System.out.print("Harga Tiket Rp."+harga);
+              }
+            }
+            
+            omset=omset+harga;
+            
+            kursi[bariskursi][kolomkursi]=1;
+            
+            break;
+          case 2: 
+            System.out.println("Baris/Kolom\tA\tB\tC\tD\tE\tF");
+            for(int i=0;i<13;i++)
+            {
+              System.out.print("baris "+baris[i]+"\t");
+              for(int j=0;j<6;j++)
+              {
+                if(kursi[i][j]==0)
+                {
+                  System.out.print("*\t");
+                }
+                else
+                {
+                  System.out.print("X\t");
+                }
+              }
+              System.out.println();
+            }
+            System.out.println();
+            
+            break;
+          case 3:
+            int vip = 0;
+            for (int a = 0 ; a < 2 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 1)
+                {
+                  vip++;
+                }
+              }
+            }
+            
+             int bisnis= 0;
+            for (int a = 2 ; a <=6 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 1)
+                {
+                  bisnis++;
+                }
+              }
+            }
+            
+            int ekonomi= 0;
+            for (int a = 7 ; a <13 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 1)
+                {
+                  ekonomi++;
+                }
+              }
+            }
+            
+            int terisi= 0;
+            for (int a = 0 ; a <13 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 1)
+                {
+                  terisi++;
+                }
+              }
+            }
+            
+            System.out.println("Jumlah kursi terisi : " +terisi);
+            System.out.println("Jumlah kursi vip  : " +vip);
+            System.out.println("Jumlah kursi bisnis : " +bisnis);
+            System.out.println("Jumlah kursi ekonomi : " +ekonomi);
+            break;
+          case 4: 
+            int kosong = 0;
+            for (int a = 0 ; a < 13 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 0)
+                {
+                  kosong++;
+                }
+              }
+            }
+            
+            int vipk = 0;
+            for (int a = 0 ; a < 2 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 0)
+                {
+                  vipk++;
+                }
+              }
+            }
+            
+             int bisnisk= 0;
+            for (int a = 2 ; a <=6 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 0)
+                {
+                  bisnisk++;
+                }
+              }
+            }
+            
+            int ekonomik= 0;
+            for (int a = 7 ; a <13 ; a++)
+            {
+              for (int b = 0; b < 6; b++ )
+              {
+                if (kursi[a][b] == 0)
+                {
+                  ekonomik++;
+                }
+              }
+            }
+            
+            System.out.println("Jumlah kursi kosong : " +kosong);
+            System.out.println("Jumlah kursi vip kosong : " +vipk);
+            System.out.println("Jumlah kursi bisnis kosong : " +bisnisk);
+            System.out.println("Jumlah kursi ekonomi kosong : " +ekonomik);
+            break; 
+          case 5:
+            System.out.print("Total Omset : "+omset);
+            break;
+          case 6:
+            System.exit(0);
+            break;
+        }
+        System.out.println();
+      }    
+      while(true);
+    }
+    catch(Exception e)
     {
-      if(bariskursi==1||bariskursi==2)
-      {
-        System.out.print("Harga Tiket : Rp.175.000");
-      }
-      else if(bariskursi>=3&&bariskursi<=7)
-      {
-        System.out.print("Harga Tiket Rp.150.000");
-      }
-      else
-      {
-        System.out.print("Harga Tiket Rp.100.000");
-      }
+      System.out.print("Inputan Anda Salah atau Kursi yang Anda Pilih Tidak Tersedia");
     }
-    break;
-    case 2: 
-      int kosong = 0;
- for (int a = 0 ; a < 13 ; a++)
- {
-  for (int b = 0; b < 6; b++ )
-  {
-   if (kursi[a][b] == 0)
-   {
-   kosong++;
-   }
-  }
- }
- System.out.println("Jumlah kursi kosong : " +kosong);
-    break;
-    case 3:
-       int terisi = 0;
- for (int a = 0 ; a < 13 ; a++)
- {
-  for (int b = 0; b < 6; b++ )
-  {
-   if (kursi[a][b] == 1)
-   {
-   terisi++;
-   }
-  }
- }
- System.out.println("Jumlah kursi yang sudah terisi : " +terisi);
-    break;
-    case 4: 
-       int semua = 0;
- for (int a = 0 ; a < 13 ; a++)
- {
-  for (int b = 0; b < 6; b++ )
-  {
-   if (kursi[a][b] == 1||kursi[a][b] == 0)
-   {
-   semua++;
-   }
-  }
- }
- System.out.println("Jumlah semua kursi: " +semua);
-    break; 
-    case 5:
-      int jum=0;
-      for(int i=0;i<13;i++)
-    {
-    for(int j=0;j<6;j++)
-    {
-      if(kursi[i][j]==1)
-      {
-        jum++;
-      }
-    }
-    omset=jum*150000;   
-    }
-     System.out.print("Total Omset : "+omset);
-    break;
-    case 6:
-      System.exit(0);
-    break;
-    }
-    System.out.println();
-    }    
-    while(true);
-   
   }
 }
